d71811cdf5bf4e80032e7f53e8a25b38664bb32a
Silx79 <javier.mira46@gmail.com>
2023-11-05 20:16:20 +0100

    Menu casi finalizado. Revisar las condiciones de salida y dar opcion de preguntar si quiere registrar un nuevo usuario.

--- src/main/kotlin/Clases/Empresa.kt
+++ src/main/kotlin/Clases/Empresa.kt
@@ -2,80 +2,130 @@ package Clases
 
 class Empresa() {
 
+
     val listaTrabajadores = ArrayList<Trabajadores>()
     val listaJefes = ArrayList<Jefes>()
     val listaAsalariados = ArrayList<Asalariados>()
     val listaAutonomos = ArrayList<Autonomos>()
 
-    fun registroTrabajadores() {
 
+    fun registrarTrabajador(): ArrayList<Trabajadores> {
 
         println("Indique el tipo de trabajador que quiere registrar:")
         println("1.Jefes.")
         println("2.Asalariados.")
         println("3.Autonomos.")
+        println("4.Salir.")
 
+        var salir = false
         val seleccion = readln().toInt()
 
-        when (seleccion) {
+        while (!salir) {
+
+            when (seleccion) {
 
 
-            1 -> {
+                1 -> {
+                    println("Introduzca el nombre:")
+                    var nombre = readln()
+                    println("Introduzca el apellido:")
+                    var apellidos = readln()
+                    println("Introduzca el DNI:")
+                    var dni = readln()
+                    println("Introduzca el numero de acciones:")
+                    var acciones = readln().toInt()
+                    println("Introduzca el beneficio obtenido:")
+                    var beneficio = readln().toInt()
+
+                    val jefe: Trabajadores =
+                        Jefes(nombre, apellidos, dni, acciones, beneficio)
+
+                    listaJefes.add(jefe as Jefes)
+                    listaTrabajadores.add(jefe)
+                    println("Trabajador registrado con exito.")
+
+                    println("Datos del trabajador registrado:\n" + jefe.mostrarDatos())
+                    return listaTrabajadores
 
 
-                println("Introduzca el nombre:")
-                var nombre = readln()
-                println("Introduzca el apellido:")
-                var apellidos = readln()
-                println("Introduzca el DNI:")
-                var dni = readln()
-                println("Introduzca el numero de acciones:")
-                var acciones = readln().toInt()
-                println("Introduzca el beneficio obtenido:")
-                var beneficio = readln().toInt()
+                }
 
-                val jefe: Trabajadores =
-                    Jefes(nombre, apellidos, dni, acciones, beneficio)
-                listaJefes.add(jefe as Jefes)
-                listaTrabajadores.add(jefe)
-                println("Trabajador registrado con exito.")
+                2 -> {
+                    println("Introduzca el nombre:")
+                    var nombre = readln()
+                    println("Introduzca el apellido:")
+                    var apellidos = readln()
+                    println("Introduzca el DNI:")
+                    var dni = readln()
+                    println("Introduzca el sueldo anual:")
+                    var sueldo = readln().toInt()
+                    println("Introduzca el numero de pagas:")
+                    var nPagas = readln().toInt()
+                    var contratado = true
 
-                println("Datos del trabajador registrado:\n " + jefe.mostrarDatos())
+                    val asalariado: Trabajadores = Asalariados(nombre, apellidos, dni, sueldo, nPagas, contratado)
+
+                    listaAsalariados.add(asalariado as Asalariados)
+                    listaTrabajadores.add(asalariado)
+
+                    println("Trabajador registrado con exito.")
+
+                    println("Datos del trabajador registrado:\n" + asalariado.mostrarDatos())
 
 
+                }
+
+                3 -> {
+
+
+                    println("Introduzca el nombre:")
+                    var nombre = readln()
+                    println("Introduzca el apellido:")
+                    var apellidos = readln()
+                    println("Introduzca el DNI:")
+                    var dni = readln()
+                    println("Introduzca el sueldo anual:")
+                    var sueldo = readln().toInt()
+
+
+                    var contratado = true
+
+                    val autonomo: Trabajadores = Autonomos(nombre, apellidos, dni, sueldo, contratado)
+
+                    listaAutonomos.add(autonomo as Autonomos)
+                    listaTrabajadores.add(autonomo)
+
+                    println("Trabajador registrado con exito.")
+                    println("Datos del trabajador registrado:\n" + autonomo.mostrarDatos())
+
+
+                }
+
+                4 -> salir = true
+
+                else -> println("Introduzca una opcion valida.")
             }
-
-            2 -> {
-
-
-                println("Introduzca el nombre:")
-                var nombre = readln()
-                println("Introduzca el apellido:")
-                var apellidos = readln()
-                println("Introduzca el DNI:")
-                var dni = readln()
-                println("Introduzca el sueldo anual:")
-                var sueldo = readln().toInt()
-                println("Introduzca el numero de pagas:")
-                var nPagas = readln().toInt()
-                var contratado = true
-
-                val asalariado: Trabajadores = Asalariados(nombre, apellidos, dni, sueldo, nPagas, contratado)
-
-                listaAsalariados.add(asalariado as Asalariados)
-                listaTrabajadores.add(asalariado)
-
-                println("Trabajador registrado con exito.")
-
-                println("Datos del trabajador registrado:\n " + asalariado.mostrarDatos())
-
-            }
-
-
         }
+        return listaTrabajadores
+    }
 
+    fun listarTrabajadores() {
+
+        println("Lista de Jefes: ")
+        listaJefes.forEach { e -> println(e.mostrarDatos()) }
+
+        println("Lista de Asalariados: ")
+        listaAsalariados.forEach { e -> println(e.mostrarDatos()) }
+
+        println("Lista de Autonomos: ")
+        listaAutonomos.forEach { e -> println(e.mostrarDatos()) }
 
     }
 
 
 }
+
+
+
+
+
